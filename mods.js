1868ae0a4f2240dae5f1af5b6e91cea9655bb32b
MKJJR <maikotavares123456789@gmail.com>
2026-01-26 13:37:37 -0300

    Atualizar o mods.js

--- mods.js
+++ mods.js
@@ -15,5 +15,13 @@ const listaDeMods = [
         descricao: "O clássico Fusca com suspensão a ar e som forte.",
         imagem: "mods/img/fusca.png", // certifique-se que a imagem existe
         link: "mods/fusca-rebaixado.html"
-    }
+    },
+    {
+        id: "gasolina",
+        categoria: "script",
+        titulo: "Mod Gasolina Realista",
+        descricao: "Adiciona um sistema de combustível realista nos postos.",
+        imagem: "mods/img/mod-gasolina.png",
+        link: "mods/mod-gasolina.html"
+    },
 ];
