db3f03e4907ec07c8acb42c4544ea934257f489f
MKJJR <maikotavares123456789@gmail.com>
2026-01-26 13:16:35 -0300

    Criar mods.js

---
+++ mods.js
@@ -0,0 +1,19 @@
+// Arquivo: mods.js
+const listaDeMods = [
+    {
+        id: "gasolina",
+        categoria: "script",
+        titulo: "Mod Gasolina Realista",
+        descricao: "Adiciona um sistema de combustível realista nos postos.",
+        imagem: "mods/img/mod-gasolina.png",
+        link: "mods/mod-gasolina.html"
+    },
+    {
+        id: "fusca",
+        categoria: "veiculo",
+        titulo: "Fusca Rebaixado",
+        descricao: "O clássico Fusca com suspensão a ar e som forte.",
+        imagem: "mods/img/fusca.png", // certifique-se que a imagem existe
+        link: "mods/fusca-rebaixado.html"
+    }
+];
